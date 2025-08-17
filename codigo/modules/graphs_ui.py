7ff8db39b07188070e3e89440310e4dc1b27fcfe
Alejandro Rivera Casaseca <78324753+redalex73@users.noreply.github.com>
2025-08-17 13:35:11 +0200

    Delete codigo directory

--- codigo/modules/graphs_ui.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
-
-class GraphsTab(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("Aquí se mostrarán las gráficas del progreso"))
-        self.setLayout(layout)
